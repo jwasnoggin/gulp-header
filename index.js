ae56969edf133d094260f3fb9881d234fc8dd2e7
David Ahern <david.ahern@emc.com>
2016-06-17 13:02:22 +0100

    Added try catch to directory check

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 'use strict';
 
 /**
-* Module dependencies.
-*/
+ * Module dependencies.
+ */
 
 var Concat = require('concat-with-sourcemaps');
 var extend = require('object-assign');
@@ -14,8 +14,8 @@ var path = require('path');
 var fs = require('fs');
 
 /**
-* gulp-header plugin
-*/
+ * gulp-header plugin
+ */
 
 module.exports = function (headerText, data) {
   headerText = headerText || '';
@@ -35,7 +35,15 @@ module.exports = function (headerText, data) {
     var template = data === false ? headerText : gutil.template(headerText, extend({ file: file, filename: filename }, data));
     concat = new Concat(true, filename);
 
-    if (fs.lstatSync(file.path).isDirectory()) {
+    var isDirectory = false;
+
+    try {
+      isDirectory = fs.lstatSync(file.path).isDirectory();
+    } catch(e) {
+      isDirectory = false;
+    }
+
+    if (isDirectory) {
       // make sure the file goes through the next gulp plugin
       this.push(file);
 
@@ -81,8 +89,8 @@ module.exports = function (headerText, data) {
 };
 
 /**
-* is stream?
-*/
+ * is stream?
+ */
 
 function isStream(obj) {
   return obj instanceof stream.Stream;
