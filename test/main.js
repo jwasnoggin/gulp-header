df5af418ab7fba26eef5f4150781e6b4ccf23264
Michael J. Ryan <mryan@runbeck.net>
2018-12-12 17:23:47 -0700

    remove event-stream from the package, only used for testing

--- test/main.js
+++ test/main.js
@@ -6,21 +6,32 @@ var header = require('../');
 var should = require('should');
 var fs = require('fs');
 var path = require('path');
-var es = require('event-stream');
 var stream = require('stream');
 var File = require('vinyl');
 var gulp = require('gulp');
 require('mocha');
 
+const streamToString = stream =>
+  new Promise((resolve, reject) => {
+    try {
+      const chunks = [];
+      stream.on('data', chunk => chunks.push(chunk));
+      stream.on('error', reject);
+      stream.on('end', _ => resolve(Buffer.concat(chunks).toString('utf8')));
+    } catch (err) {
+      reject(err);
+    }
+  });
+
 describe('gulp-header', function() {
   var fakeFile;
 
-  function getFakeFile(fileContent, data){
+  function getFakeFile(fileContent, data) {
     var result = new File({
       path: './test/fixture/file.txt',
       cwd: './test/',
       base: './test/fixture/',
-      contents: new Buffer(fileContent || '')
+      contents: Buffer.from(fileContent || ''),
     });
     if (data !== undefined) {
       result.data = data;
@@ -28,34 +39,37 @@ describe('gulp-header', function() {
     return result;
   }
 
-  function getFakeFileReadStream(){
+  function getFakeFileReadStream() {
+    var s = new stream.Readable({ objectMode: true });
+    s._read = () => {};
+    s.push('Hello world');
+    s.push(null);
     return new File({
-      contents: new stream.Readable({objectMode: true}).wrap(es.readArray(['Hello world'])),
-      path: './test/fixture/anotherFile.txt'
+      contents: s,
+      path: './test/fixture/anotherFile.txt',
     });
   }
 
-  beforeEach(function(){
+  beforeEach(function() {
     fakeFile = getFakeFile('Hello world');
   });
 
   describe('header', function() {
-
     it('file should pass through', function(done) {
       var file_count = 0;
       var stream = header();
-      stream.on('data', function(newFile){
+      stream.on('data', function(newFile) {
         should.exist(newFile);
         should.exist(newFile.path);
         should.exist(newFile.relative);
         should.exist(newFile.contents);
-        newFile.path.should.equal('test/fixture/file.txt');
+        newFile.path.should.equal('test/fixture/file.txt'.split('/').join(path.sep));
         newFile.relative.should.equal('file.txt');
         newFile.contents.toString('utf8').should.equal('Hello world');
         ++file_count;
       });
 
-      stream.once('end', function () {
+      stream.once('end', function() {
         file_count.should.equal(1);
         done();
       });
@@ -83,20 +97,19 @@ describe('gulp-header', function() {
 
       myHeader.write(getFakeFileReadStream());
 
-      myHeader.once('data', function(file) {
+      myHeader.once('data', async function(file) {
         should(file.isStream()).ok;
-        file.contents.pipe(es.wait(function(err, data) {
-          data.toString('utf8').should.equal('And then i said : Hello world');
-          done();
-        }));
+        const result = await streamToString(file.contents);
+        result.should.equal('And then i said : Hello world');
+        done();
       });
       myHeader.end();
     });
 
     it('should format the header', function(done) {
-      var stream = header('And then <%= foo %> said : ', { foo : 'you' } );
+      var stream = header('And then <%= foo %> said : ', { foo: 'you' });
       //var stream = header('And then ${foo} said : ', { foo : 'you' } );
-      stream.on('data', function (newFile) {
+      stream.on('data', function(newFile) {
         should.exist(newFile.contents);
         newFile.contents.toString('utf8').should.equal('And then you said : Hello world');
       });
@@ -107,8 +120,8 @@ describe('gulp-header', function() {
     });
 
     it('should format the header (ES6 delimiters)', function(done) {
-      var stream = header('And then ${foo} said : ', { foo : 'you' } );
-      stream.on('data', function (newFile) {
+      var stream = header('And then ${foo} said : ', { foo: 'you' });
+      stream.on('data', function(newFile) {
         should.exist(newFile.contents);
         newFile.contents.toString('utf8').should.equal('And then you said : Hello world');
       });
@@ -119,13 +132,12 @@ describe('gulp-header', function() {
     });
 
     it('should access to the current file', function(done) {
-      var stream = header([
-        '<%= file.relative %>',
-        '<%= file.path %>',
-        ''].join('\n'));
-      stream.on('data', function (newFile) {
+      var stream = header(['<%= file.relative %>', '<%= file.path %>', ''].join('\n'));
+      stream.on('data', function(newFile) {
         should.exist(newFile.contents);
-        newFile.contents.toString('utf8').should.equal('file.txt\ntest/fixture/file.txt\nHello world');
+        newFile.contents
+          .toString('utf8')
+          .should.equal('file.txt\ntest/fixture/file.txt\nHello world'.split('/').join(path.sep));
       });
       stream.once('end', done);
 
@@ -135,20 +147,20 @@ describe('gulp-header', function() {
 
     it('should access the data of the current file', function(done) {
       var stream = header('<%= license %>\n');
-      stream.on('data', function (newFile) {
+      stream.on('data', function(newFile) {
         should.exist(newFile.contents);
         newFile.contents.toString('utf8').should.equal('WTFPL\nHello world');
       });
       stream.once('end', done);
 
-      stream.write(getFakeFile('Hello world', {license: "WTFPL"}));
+      stream.write(getFakeFile('Hello world', { license: 'WTFPL' }));
       stream.end();
     });
 
-    it('multiple files should pass through', function (done) {
+    it('multiple files should pass through', function(done) {
       var headerText = 'use strict;',
-          stream = gulp.src('./test/fixture/*.txt').pipe(header(headerText)),
-          files = [];
+        stream = gulp.src('./test/fixture/*.txt').pipe(header(headerText)),
+        files = [];
 
       stream.on('error', done);
       stream.on('data', function(file) {
@@ -161,10 +173,10 @@ describe('gulp-header', function() {
       });
     });
 
-    it('no files are acceptable', function (done) {
+    it('no files are acceptable', function(done) {
       var headerText = 'use strict;',
-          stream = gulp.src('./test/fixture/*.html').pipe(header(headerText)),
-          files = [];
+        stream = gulp.src('./test/fixture/*.html').pipe(header(headerText)),
+        files = [];
 
       stream.on('error', done);
       stream.on('data', function(file) {
@@ -176,5 +188,4 @@ describe('gulp-header', function() {
       });
     });
   });
-
 });
